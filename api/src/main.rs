5e2d26f46e5c5306f128efc1276805589c193126
jan <J.pawlowitz@t-online.de>
2023-10-25 15:23:02 +0200

    basic http server setup

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::{get}, ServiceExt};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/test", get(get_test));
+
+    axum::Server::bind(&"0.0.0.0:3010".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn get_test() -> &'static str {
+    "test"
 }
