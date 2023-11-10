1fc0983cccb44f368a0c35828e3c7c0a46b2c84c
jan <J.pawlowitz@t-online.de>
2023-11-10 14:04:34 +0100

    basic recipe fetch implemented

--- api/src/db_handler/recipes.rs
+++ api/src/db_handler/recipes.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, MySqlPool, query_as};
 
 pub mod routes;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Recipe {
     name: String,
     description: String,
-    ingredients: Vec<Ingredient>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,7 +16,16 @@ pub struct Ingredient {
     unit: String,
 }
 
-#[derive(Deserialize)]
-pub struct RecipeRequest {
-    ingredients: Vec<String>
+pub async fn get_recipes_from_ingredients(pool: &MySqlPool, ingredients: &str) -> Result<Vec<Recipe>, anyhow::Error> {
+    let recipes = query_as::<_, Recipe>(
+        "SELECT recipes.name, recipes.description FROM recipes \
+        INNER JOIN recipe_ingredients ON recipes.id = recipe_ingredients.recipe_id \
+        INNER JOIN ingredients \
+        ON ingredients.id = recipe_ingredients.ingredient_id \
+        WHERE ingredients.name= ? ")
+        .bind("nudeln")
+        .fetch_all(pool)
+        .await?;
+
+    Ok(recipes)
 }
--- api/src/db_handler/recipes/routes.rs
+++ api/src/db_handler/recipes/routes.rs
@@ -1,12 +1,28 @@
-use axum::{Json, response::{IntoResponse, Response}, http::StatusCode, Router, routing::post};
+use axum::{Json, response::{IntoResponse, Response}, http::StatusCode, Router, routing::post, Extension};
+use serde::Deserialize;
+use sqlx::MySqlPool;
+use crate::db_handler::recipes::get_recipes_from_ingredients;
 
-use super::RecipeRequest;
+#[derive(Deserialize)]
+pub struct RecipeRequest {
+    ingredients: Vec<String>
+}
 
 pub fn get_recipe_routes() -> Router {
     Router::new()
         .route("/get", post(handle_get_recipes))
 }
 
-pub async fn handle_get_recipes(Json(ingredients): Json<RecipeRequest>) -> Response {
-    (StatusCode::OK).into_response()
+pub async fn handle_get_recipes(Extension(pool): Extension<MySqlPool>, Json(ingredients): Json<RecipeRequest>) -> Response {
+    let recipes = get_recipes_from_ingredients(&pool, "nudeln").await;
+
+    match recipes {
+        Ok(recipes) => {
+            Json(recipes).into_response()
+        },
+        Err(e) => {
+            println!("Error fetching recipes: {}", e.to_string());
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
 }
