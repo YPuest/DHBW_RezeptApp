2f64c4af27575e97d02e7337e4c324da2e5fad62
jan <J.pawlowitz@t-online.de>
2023-11-29 12:04:28 +0100

    added user creation and user auth endpoints

--- api/src/db_handler/users/routes.rs
+++ api/src/db_handler/users/routes.rs
@@ -1,12 +1,33 @@
-use axum::{Json, response::{IntoResponse, Response}, http::StatusCode, Router, routing::post};
+use axum::{Json, response::{IntoResponse, Response}, http::StatusCode, Router, routing::post, Extension};
+use sqlx::MySqlPool;
 
-use super::User;
+use super::{check_auth, create_user, User};
 
 pub fn get_user_routes() -> Router {
     Router::new()
         .route("/create", post(handle_create_user))
+        .route("/auth", post(handle_check_auth))
 }
 
-pub async fn handle_create_user(Json(user): Json<User>) -> Response {
-    (StatusCode::OK).into_response()
+pub async fn handle_create_user(Extension(pool): Extension<MySqlPool>, Json(user): Json<User>) -> Response {
+    match create_user(&pool, &user).await {
+        Ok(()) => (StatusCode::OK).into_response(),
+        Err(e) => {
+            println!("Error creating user: {}", e.to_string());
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
+}
+
+pub async fn handle_check_auth(Extension(pool): Extension<MySqlPool>, Json(user): Json<User>) -> Response {
+    match check_auth(&pool, &user).await {
+        Ok(auth_check) => match auth_check {
+            true => StatusCode::OK.into_response(),
+            false => StatusCode::UNAUTHORIZED.into_response()
+        },
+        Err(e) => {
+            println!("Error checking user auth: {}", e.to_string());
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
 }
